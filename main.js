ebf28decdfc38c7cb5720ec8e6e96031ae20428d
Hermes (agora) <hermes.frangoudis@agora.io>
2024-05-10 17:41:01 -0400

    initial UI set up

--- main.js
+++ main.js
@@ -1,24 +1,66 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const contianer = document.getElementById('container')
+let divCount = 0
 
-setupCounter(document.querySelector('#counter'))
+document.body.addEventListener('keydown', event => {
+  console.log(`key: ${event.code}`)
+  if (event.code == 'Space')
+    addDiv(divCount)
+})
+
+document.body.addEventListener('click', event => {
+  const div = event.target
+  if (parseInt(div.id) % 2 == 0) {
+    addWiggleAnimation(div)
+  } else {
+    addMorphAnimation(div)
+  }
+  
+})
+
+const addDiv = (id) => {
+  console.log(`create div: ${id}`)
+  const div = document.createElement('div')
+  div.className = 'user'
+  div.id = id
+  div.textContent = `0x${id}${++divCount}` // replace with Id
+
+  // create random background color
+  const hue = Math.random() * 360
+  const saturation =  Math.random() * 100
+  const lightness = (Math.random() * 60) + 20
+  div.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+
+  // calculate complimentary colors for the text
+  const complimentHue = (hue + 180) * 360
+  const complimentLightness = lightness < 50 ? 80 : 20
+  div.style.color = `hsl(${complimentHue}, ${saturation}%, ${complimentLightness}%)` 
+
+  contianer.appendChild(div)
+  adjustGrid()
+}
+
+const adjustGrid = () => {
+  const divs = contianer.querySelectorAll('.user')
+  const numDivs = divs.length
+  let cols = Math.ceil(Math.sqrt(numDivs))
+  let rows = Math.ceil(numDivs/cols)
+
+  contianer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+  contianer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+}
+
+const addWiggleAnimation = (div) => {
+  div.classList.add('wiggle-animation')
+  setTimeout(() => {
+    div.classList.remove('wiggle-animation')
+  }, 6000)
+}
+
+const addMorphAnimation = (div) => {
+  div.classList.add('morph-animation')
+  setTimeout(() => {
+    div.classList.remove('morph-animation')
+  }, 2000)
+}
